354f73a76a249456922459787720816cbfaed13b
Cole Bemis <colebemis@github.com>
2021-08-17 22:39:45 +0000

    Add index.js

---
+++ src/configs/recommended.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: [['primer-react/no-deprecated-colors', 'warn']]
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'no-deprecated-colors': require('./rules/no-deprecated-colors')
+  },
+  configs: {
+    recommended: require('./configs/recommended')
+  }
+}
