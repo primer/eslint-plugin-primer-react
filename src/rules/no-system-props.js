4dc79f0ec6fe7e415b4c3a861eb0955a90bd7848
Alexis Rico <sferadev@gmail.com>
2021-11-25 13:59:47 +0100

    Add circle octicon to excluded components

--- src/rules/no-system-props.js
+++ src/rules/no-system-props.js
@@ -14,6 +14,7 @@ const utilityComponents = new Set(['Box', 'Text'])
 const excludedComponentProps = new Map([
   ['AnchoredOverlay', new Set(['width', 'height'])],
   ['Avatar', new Set(['size'])],
+  ['CircleOcticon', new Set(['size'])]
   ['Dialog', new Set(['width', 'height'])],
   ['ProgressBar', new Set(['bg'])],
   ['Spinner', new Set(['size'])],
