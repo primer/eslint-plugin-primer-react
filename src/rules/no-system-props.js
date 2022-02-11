5a89de7cf49f10606192e02d99872fef545d3425
Mary White <marywhite@github.com>
2022-02-11 10:00:41 -0600

    Update no-system-props.js
    Add IconButton `size` to excluded component props

--- src/rules/no-system-props.js
+++ src/rules/no-system-props.js
@@ -18,7 +18,8 @@ const excludedComponentProps = new Map([
   ['ProgressBar', new Set(['bg'])],
   ['Spinner', new Set(['size'])],
   ['StyledOcticon', new Set(['size'])],
-  ['PointerBox', new Set(['bg'])]
+  ['PointerBox', new Set(['bg'])],
+  ['IconButton', new Set(['size'])]
 ])
 
 const alwaysExcludedProps = new Set(['variant'])
