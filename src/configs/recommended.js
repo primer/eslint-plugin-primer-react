f83a9803a1eb06fbbd0206c2d5d46ca8139d8dc5
Kate Higa <khiga8@github.com>
2022-07-21 14:30:06 -0700

    Recommend 'github' plugin and provide comopnent mapping

--- src/configs/recommended.js
+++ src/configs/recommended.js
@@ -5,9 +5,18 @@ module.exports = {
       jsx: true
     }
   },
-  plugins: ['primer-react'],
+  plugins: ['primer-react', 'github'],
+  extends: ['plugin:github/react'],
   rules: {
     'primer-react/no-deprecated-colors': 'warn',
     'primer-react/no-system-props': 'warn'
+  },
+  settings: {
+    github: {
+      components: {
+        Link: { as: { undefined: 'a', 'button': 'button'}},
+        Button: { default: 'button' },
+      }
+    }
   }
 }
