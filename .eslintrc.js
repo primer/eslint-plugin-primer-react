c520d0882fbb6e36abf885b1a3729435cd0b7b4a
Josh Black <joshblack@github.com>
2023-10-11 12:21:41 -0500

    chore(eslint): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,36 @@
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:github/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true
+  extends: ['eslint:recommended', 'plugin:github/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  env: {
+    commonjs: true,
+    node: true
+  },
+  rules: {
+    'import/no-commonjs': 'off',
+    'no-shadow': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_'
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true
+      }
+    },
+    {
+      files: ['.eslintrc.js'],
+      rules: {
+        'filenames/match-regex': 'off'
+      }
+    }
+  ]
 }
