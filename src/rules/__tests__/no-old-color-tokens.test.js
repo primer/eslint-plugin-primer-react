1396946ffdcf6b915d1cdfd9bb755662c3d080f5
Lukas Oppermann <lukasoppermann@github.com>
2023-11-16 16:48:46 +0100

    add rule

---
+++ src/rules/__tests__/no-old-color-tokens.test.js
@@ -0,0 +1,344 @@
+const rule = require('../new-color-css-vars')
+const {RuleTester} = require('eslint')
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+})
+
+ruleTester.run('no-color-css-vars', rule, {
+  valid: [
+    {
+      code: `{color: 'fg.default'}`,
+    },
+    {
+      code: `<circle style={{color: '#444'}} strokeWidth="2" />`,
+    },
+    {
+      code: `<circle stroke="var(--borderColor-default)" strokeWidth="2" />`,
+    },
+    {
+      code: `<circle fill="red" strokeWidth="2" />`,
+    },
+    {
+      code: `<Blankslate border></Blankslate>`,
+    },
+    {
+      code: `<div sx={{lineHeight: 1}}></div>`,
+    },
+    {
+      name: 'variable with number',
+      code: `
+        const size = 2
+        export const Fixture = <Button padding={size}>Test</Button>
+      `,
+    },
+  ],
+  invalid: [
+    {
+      name: 'attribute: simple variable',
+      code: `<circle stroke="var(--color-border-default)" fill="var(--color-border-default)" strokeWidth="2" />`,
+      output: `<circle stroke="var(--borderColor-default, var(--color-border-default))" fill="var(--borderColor-default, var(--color-border-default))" strokeWidth="2" />`,
+      errors: [
+        {
+          message: 'Replace var(--color-border-default) with var(--borderColor-default, var(--color-border-default))',
+        },
+        {
+          message: 'Replace var(--color-border-default) with var(--borderColor-default, var(--color-border-default))',
+        },
+      ],
+    },
+    {
+      name: 'attribute: conditional variable',
+      code: `<circle stroke={test ? "var(--color-border-default)" : "red"} strokeWidth="2" />`,
+      output: `<circle stroke={test ? "var(--borderColor-default, var(--color-border-default))" : "red"} strokeWidth="2" />`,
+      errors: [
+        {
+          message: 'Replace var(--color-border-default) with var(--borderColor-default, var(--color-border-default))',
+        },
+      ],
+    },
+    {
+      name: 'sx: simple variable',
+      code: `<Button sx={{color: 'var(--color-fg-muted)'}}>Test</Button>`,
+      output: `<Button sx={{color: "var(--fgColor-muted, var(--color-fg-muted))"}}>Test</Button>`,
+      errors: [
+        {
+          message: 'Replace var(--color-fg-muted) with var(--fgColor-muted, var(--color-fg-muted))',
+        },
+      ],
+    },
+    {
+      name: 'style: simple variable',
+      code: `<div style={{ border: 'var(--color-border-default)' }}></div>`,
+      output: `<div style={{ border: "var(--borderColor-default, var(--color-border-default))" }}></div>`,
+      errors: [
+        {
+          message: 'Replace var(--color-border-default) with var(--borderColor-default, var(--color-border-default))',
+        },
+      ],
+    },
+    {
+      name: 'sx: nested variable',
+      code: `
+        <Box sx={{
+          '&:hover button, &:focus [data-component="copy-link"] button': {
+            color: 'var(--color-accent-fg)'
+          }
+        }}>
+        </Box>`,
+      output: `
+        <Box sx={{
+          '&:hover button, &:focus [data-component="copy-link"] button': {
+            color: "var(--fgColor-accent, var(--color-accent-fg))"
+          }
+        }}>
+        </Box>`,
+      errors: [
+        {
+          message: 'Replace var(--color-accent-fg) with var(--fgColor-accent, var(--color-accent-fg))',
+        },
+      ],
+    },
+    {
+      name: 'style: nested variable',
+      code: `
+        <Box style={{
+          '&:hover button, &:focus [data-component="copy-link"] button': {
+            color: 'var(--color-accent-fg)'
+          }
+        }}>
+        </Box>`,
+      output: `
+        <Box style={{
+          '&:hover button, &:focus [data-component="copy-link"] button': {
+            color: "var(--fgColor-accent, var(--color-accent-fg))"
+          }
+        }}>
+        </Box>`,
+      errors: [
+        {
+          message: 'Replace var(--color-accent-fg) with var(--fgColor-accent, var(--color-accent-fg))',
+        },
+      ],
+    },
+    {
+      name: 'value variable in scope',
+      code: `
+        const bg = 'var(--color-canvas-subtle)'
+        export const Fixture = <Button bg={bg}>Test</Button>
+      `,
+      output: `
+        const bg = "var(--bgColor-muted, var(--color-canvas-subtle))"
+        export const Fixture = <Button bg={bg}>Test</Button>
+      `,
+      errors: [
+        {
+          message: 'Replace var(--color-canvas-subtle) with var(--bgColor-muted, var(--color-canvas-subtle))',
+        },
+      ],
+    },
+    {
+      name: 'variable object in scope',
+      code: `
+        const baseStyles = { color: 'var(--color-fg-muted)' }
+        export const Fixture = <Button sx={baseStyles}>Test</Button>
+      `,
+      output: `
+        const baseStyles = { color: "var(--fgColor-muted, var(--color-fg-muted))" }
+        export const Fixture = <Button sx={baseStyles}>Test</Button>
+      `,
+      errors: [
+        {
+          message: 'Replace var(--color-fg-muted) with var(--fgColor-muted, var(--color-fg-muted))',
+        },
+      ],
+    },
+    {
+      name: 'merge in sx',
+      code: `
+        import {merge} from '@primer/react'
+        export const Fixture = props => <Button sx={merge({color: 'var(--color-fg-muted)'}, props.sx)}>Test</Button>
+      `,
+      output: `
+        import {merge} from '@primer/react'
+        export const Fixture = props => <Button sx={merge({color: "var(--fgColor-muted, var(--color-fg-muted))"}, props.sx)}>Test</Button>
+      `,
+      errors: [
+        {
+          message: 'Replace var(--color-fg-muted) with var(--fgColor-muted, var(--color-fg-muted))',
+        },
+      ],
+    },
+    // {
+    //   code: `<Box sx={{borderColor: 'var(--color-border-default)'}} />`,
+    //   output: `<Box sx={{borderColor: 'var(--borderColor-default, var(--color-border-default))'}} />`,
+    //   errors: [
+    //     {
+    //       message: 'Replace var(--color-border-default) with var(--borderColor-default, var(--color-border-default))'
+    //     }
+    //   ]
+    // },
+    {
+      name: 'variable in styled.component',
+      code: `
+        import {sx, SxProp} from '@primer/react'
+        export const HighlightToken = styled.span\`
+          color: var(--color-accent-emphasis);
+          \${sx}
+        \`
+        const ClickableTokenSpan = styled(HighlightToken)\`
+          &:hover, &:focus { background-color: accent.muted;}
+        \`
+      `,
+      errors: [
+        {
+          message: 'Replace var(--color-accent-emphasis) with var(--fgColor-accent, var(--color-accent-emphasis))',
+        },
+      ],
+    },
+    {
+      name: 'sx: conditional variable',
+      code: `
+        import {Box} from '@primer/react'
+
+        function someComponent({subtle}) {
+          return (
+            <Box
+              sx={{
+                boxShadow: subtle
+                  ? 'inset 2px 0 0 var(--color-fg-subtle)'
+                  : 'inset 2px 0 0 var(--color-attention-fg)',
+                color: 'var(--fgColor-default)',
+                bg: 'var(--color-canvas-default)'
+              }}
+            />
+          )
+        }
+      `,
+      output: `
+        import {Box} from '@primer/react'
+
+        function someComponent({subtle}) {
+          return (
+            <Box
+              sx={{
+                boxShadow: subtle
+                  ? "inset 2px 0 0 var(--borderColor-neutral-emphasis, var(--color-fg-subtle))"
+                  : "inset 2px 0 0 var(--bgColor-attention-emphasis, var(--color-attention-fg))",
+                color: 'var(--fgColor-default)',
+                bg: "var(--bgColor-default, var(--color-canvas-default))"
+              }}
+            />
+          )
+        }
+      `,
+      errors: [
+        {
+          message: 'Replace var(--color-fg-subtle) with var(--borderColor-neutral-emphasis, var(--color-fg-subtle))',
+        },
+        {
+          message:
+            'Replace var(--color-attention-fg) with var(--bgColor-attention-emphasis, var(--color-attention-fg))',
+        },
+        {
+          message: 'Replace var(--color-canvas-default) with var(--bgColor-default, var(--color-canvas-default))',
+        },
+      ],
+    },
+    {
+      name: 'style: conditional variable',
+      code: `
+        import {Box} from '@primer/react'
+
+        function someComponent({subtle}) {
+          return (
+            <Box
+              style={{
+                boxShadow: subtle
+                  ? 'inset 2px 0 0 var(--color-fg-subtle)'
+                  : 'inset 2px 0 0 var(--color-attention-fg)',
+                color: 'var(--fgColor-default)',
+                bg: 'var(--color-canvas-default)'
+              }}
+            />
+          )
+        }
+      `,
+      output: `
+        import {Box} from '@primer/react'
+
+        function someComponent({subtle}) {
+          return (
+            <Box
+              style={{
+                boxShadow: subtle
+                  ? "inset 2px 0 0 var(--borderColor-neutral-emphasis, var(--color-fg-subtle))"
+                  : "inset 2px 0 0 var(--bgColor-attention-emphasis, var(--color-attention-fg))",
+                color: 'var(--fgColor-default)',
+                bg: "var(--bgColor-default, var(--color-canvas-default))"
+              }}
+            />
+          )
+        }
+      `,
+      errors: [
+        {
+          message: 'Replace var(--color-fg-subtle) with var(--borderColor-neutral-emphasis, var(--color-fg-subtle))',
+        },
+        {
+          message:
+            'Replace var(--color-attention-fg) with var(--bgColor-attention-emphasis, var(--color-attention-fg))',
+        },
+        {
+          message: 'Replace var(--color-canvas-default) with var(--bgColor-default, var(--color-canvas-default))',
+        },
+      ],
+    },
+    // {
+    //   code: `<Box sx={{outline: '2px solid var(--color-accent-fg)'}}>Test</Box>`,
+    //   // output: `<Box sx={{outline: '2px solid var(--fgColor-accent, var(--borderColor-accent-emphasis, var(--focus-outlineColor, var(--color-accent-fg))))'}}>Test</Box>`,
+    //   output: `<Box sx={{outline: '2px solid var(--focus-outlineColor, var(--color-accent-fg))'}}>Test</Box>`,
+    //   errors: [
+    //     {
+    //       message: 'Replace var(--color-accent-fg) with var(--focus-outlineColor, var(--color-accent-fg))'
+    //     }
+    //   ]
+    // },
+    // {
+    //   code: `
+    //     <Box sx={{
+    //       color: 'var(--color-fg-subtle)',
+    //       '&:hover': {
+    //         color: 'var(--color-accent-fg)',
+    //       }
+    //     }}>Test</Box>
+    //   `,
+    //   // output: `
+    //   //   <Box sx={{
+    //   //     color: 'var(--fgColor-muted, var(--color-fg-subtle))',
+    //   //     '&:hover': {
+    //   //       color: 'var(--fgColor-accent, var(--color-accent-fg))',
+    //   //   }}>Test</Box>
+    //   // `,
+    //   output: `
+    //     <Box sx={{
+    //       color: 'var(--fgColor-muted, var(--color-fg-subtle))',
+    //       '&:hover': {
+    //         color: 'var(--fgColor-accent, var(--borderColor-accent-emphasis, var(--bgColor-accent-emphasis, var(--color-accent-fg))))',
+    //       }
+    //     }}>Test</Box>
+    //   `,
+    //   errors: [
+    //     {
+    //       message: 'Replace var(--color-accent-fg) with var(--focus-outlineColor, var(--color-accent-fg))'
+    //     }
+    //   ]
+    // }
+  ],
+})
---
+++ src/rules/no-old-color-tokens.js
@@ -0,0 +1,124 @@
+const cssVars = require('../utils/css-variable-map.json')
+
+const reportError = (propertyName, valueNode, context) => {
+  // performance optimisation: exit early
+  if (valueNode.type !== 'Literal') return
+  // get property value
+  const value = valueNode.value
+  // return if value is not a string
+  if (typeof value !== 'string') return
+  // return if value does not include variable
+  if (!value.includes('var(')) return
+
+  const varRegex = /var\([^)]+\)/g
+
+  const match = value.match(varRegex)
+  if (!match) return
+  const vars = match.flatMap(match =>
+    match
+      .slice(4, -1)
+      .trim()
+      .split(/\s*,\s*/g),
+  )
+
+  for (const cssVar of vars) {
+    // get the array of objects for the variable name (e.g. --color-fg-primary)
+    const cssVarObjects = cssVars[cssVar]
+    // get the object that contains the property name or the first one (default)
+    const varObjectForProp = propertyName
+      ? cssVarObjects?.find(prop => prop.props.includes(propertyName))
+      : cssVarObjects?.[0]
+    // return if no repalcement exists
+    if (!varObjectForProp?.replacement) return
+    // report the error
+    context.report({
+      node: valueNode,
+      message: `Replace var(${cssVar}) with var(${varObjectForProp.replacement}, var(${cssVar}))`,
+      fix(fixer) {
+        const fixedString = value.replaceAll(cssVar, `${varObjectForProp.replacement}, var(${cssVar})`)
+        return fixer.replaceText(valueNode, valueNode.type === 'Literal' ? `"${fixedString}"` : fixedString)
+      },
+    })
+  }
+}
+
+const reportOnObject = (node, context) => {
+  const propertyName = node.key.name
+  if (node.value?.type === 'Literal') {
+    reportError(propertyName, node.value, context)
+  } else if (node.value?.type === 'ConditionalExpression') {
+    reportError(propertyName, node.value.consequent, context)
+    reportError(propertyName, node.value.alternate, context)
+  }
+}
+
+const reportOnProperty = (node, context) => {
+  const propertyName = node.name.name
+  if (node.value?.type === 'Literal') {
+    reportError(propertyName, node.value, context)
+  } else if (node.value?.type === 'JSXExpressionContainer' && node.value.expression?.type === 'ConditionalExpression') {
+    reportError(propertyName, node.value.expression.consequent, context)
+    reportError(propertyName, node.value.expression.alternate, context)
+  }
+}
+
+const reportOnValue = (node, context) => {
+  if (node?.type === 'Literal') {
+    reportError(undefined, node, context)
+  } else if (node?.type === 'JSXExpressionContainer' && node.expression?.type === 'ConditionalExpression') {
+    reportError(undefined, node.value.expression.consequent, context)
+    reportError(undefined, node.value.expression.alternate, context)
+  }
+}
+
+// const reportOnTemplateElement = (node, context) => {
+// console.log(cssToObj(node.value.cooked))
+// console.log(node)
+// if (node?.type === 'Literal') {
+//   reportError(undefined, node, context)
+// } else if (node?.type === 'JSXExpressionContainer' && node.expression?.type === 'ConditionalExpression') {
+//   reportError(undefined, node.value.expression.consequent, context)
+//   reportError(undefined, node.value.expression.alternate, context)
+// }
+// }
+
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    hasSuggestions: true,
+    fixable: 'code',
+    docs: {
+      description: 'Upgrade legacy CSS variables to Primitives v8 in sx prop',
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          skipImportCheck: {
+            type: 'boolean',
+          },
+          checkAllStrings: {
+            type: 'boolean',
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+  },
+  /** @param {import('eslint').Rule.RuleContext} context */
+  create(context) {
+    return {
+      // sx OR style property on elements
+      ['JSXAttribute:matches([name.name=sx], [name.name=style]) ObjectExpression Property']: node =>
+        reportOnObject(node, context),
+      // variable that is an object
+      ['VariableDeclarator > ObjectExpression Property']: node => reportOnObject(node, context),
+      // property on element like stroke or fill
+      ['JSXAttribute[name.name!=sx][name.name!=style]']: node => reportOnProperty(node, context),
+      // variable that is a value
+      ['VariableDeclarator > Literal']: node => reportOnValue(node, context),
+      // variable that is a value
+      // ['VariableDeclarator TemplateElement']: node => reportOnTemplateElement(node, context),
+    }
+  },
+}
