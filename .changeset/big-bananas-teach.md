eb046249daaa9ab72d9ca4bbfba30fd494a79e23
Josh Black <joshblack@github.com>
2023-12-14 09:59:58 -0600

    feat(rules): add no-deprecated-entrypoints rule (#129)
    * feat(rules): add no-deprecated-entrypoints rule
    
    * docs: update rule docs
    
    * chore: add changeset

---
+++ .changeset/big-bananas-teach.md
@@ -0,0 +1,5 @@
+---
+'eslint-plugin-primer-react': minor
+---
+
+Add no-deprecated-entrypoints rule to lint against deprecated import usage in @primer/react
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     'direct-slot-children': require('./rules/direct-slot-children'),
     'no-deprecated-colors': require('./rules/no-deprecated-colors'),
+    'no-deprecated-entrypoints': require('./rules/no-deprecated-entrypoints'),
     'no-system-props': require('./rules/no-system-props'),
     'a11y-tooltip-interactive-trigger': require('./rules/a11y-tooltip-interactive-trigger'),
     'new-color-css-vars': require('./rules/new-color-css-vars'),
