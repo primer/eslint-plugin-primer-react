20c03358df068fd993a61868490aeca1f76c00e0
Ian Sanders <iansan5653@github.com>
2024-09-06 15:25:08 +0000

    Allow subpaths

--- src/utils/is-primer-component.js
+++ src/utils/is-primer-component.js
@@ -1,6 +1,10 @@
 const {isImportedFrom} = require('./is-imported-from')
 
-/** @returns {boolean} */
+/**
+ * Check if `name` is a JSX component that is imported from `@primer/react` or
+ * a subpath `@primer/react/*`.
+ * @returns {boolean}
+ */
 function isPrimerComponent(name, scope) {
   let identifier
 
@@ -14,7 +18,6 @@ function isPrimerComponent(name, scope) {
     default:
       return false
   }
-
-  return isImportedFrom(/^@primer\/react$/, identifier, scope)
+  return isImportedFrom(/^@primer\/react(?:$|\/)/, identifier, scope)
 }
 exports.isPrimerComponent = isPrimerComponent
