21102b7af15186e31db7783c0e2d27d5816bf317
Josh Black <joshblack@github.com>
2024-09-17 14:06:46 -0500

    feat: add SegmentedControl.Button to no-sx-prop rule

--- src/rules/no-sx-prop.js
+++ src/rules/no-sx-prop.js
@@ -3,13 +3,31 @@
 const url = require('../url')
 
 const forbidden = new Set([
-  'SegmentedControl',
   // 'ActionList',
   // 'ActionList.Divider',
   // 'ActionList.Group',
   // 'ActionList.Item',
   // 'ActionList.LeadingVisual',
   // 'ActionList.LinkItem',
+
+  // 'ActionMenu.Button',
+  // 'ActionMenu.Overlay',
+
+  // 'Avatar',
+  // 'AvatarStack',
+
+  // 'BorderBox',
+  // 'Box',
+
+  // 'BranchName',
+
+  // 'Breadcrumbs',
+  // 'Breadcrumbs.Item',
+
+  'SegmentedControl',
+  'SegmentedControl.Button',
+
+  'SplitPageLayout.Pane',
 
   // 'UnderlineNav',
   // 'UnderlineNav.Item',
@@ -36,14 +54,24 @@ module.exports = {
   create(context) {
     return {
       JSXOpeningElement(node) {
-        if (!forbidden.has(node.name.name)) {
-          return
+        if (node.name.type === 'JSXMemberExpression') {
+          if (node.name.object.type === 'JSXIdentifier' && node.name.property.type === 'JSXIdentifier') {
+            const name = `${node.name.object.name}.${node.name.property.name}`
+            if (forbidden.has(name)) {
+              context.report({
+                node,
+                messageId: 'sxProp',
+              })
+            }
+          }
+        } else if (node.name.type === 'JSXIdentifier') {
+          if (forbidden.has(node.name.name)) {
+            context.report({
+              node,
+              messageId: 'sxProp',
+            })
+          }
         }
-
-        context.report({
-          node,
-          messageId: 'sxProp',
-        })
       },
     }
   },
