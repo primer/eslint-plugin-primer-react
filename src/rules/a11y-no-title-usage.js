199637a539170adc7c83381eb43247640a742a1b
Tyler Jones <tylerjdev@github.com>
2025-02-13 09:04:57 -0500

    Small clean up

--- src/rules/a11y-no-title-usage.js
+++ src/rules/a11y-no-title-usage.js
@@ -1,18 +1,18 @@
+const url = require('../url')
 const {getJSXOpeningElementAttribute} = require('../utils/get-jsx-opening-element-attribute')
 
 module.exports = {
   meta: {
-    type: 'error', // TODO: Confirm the type
+    type: 'error',
     docs: {
       description: 'Disallow usage of title attribute on some components',
       recommended: true,
-      url: null, // TODO: Add URL
+      url: url(module),
     },
     messages: {
       noTitleOnRelativeTime: 'Avoid using the title attribute on RelativeTime.',
     },
     fixable: 'code',
-    schema: [],
   },
 
   create(context) {
@@ -25,7 +25,7 @@ module.exports = {
             node: title,
             messageId: 'noTitleOnRelativeTime',
             fix(fixer) {
-              const start = title.range[0] - 1 // Accounts for whitespace
+              const start = title.range[0] - 1
               const end = title.range[1]
               return fixer.removeRange([start, end])
             },
