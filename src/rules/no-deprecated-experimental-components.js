c709c98a84dc1ce978575136292063f3821b47cc
Marie Lucca <40550942+francinelucca@users.noreply.github.com>
2025-04-24 13:44:20 -0400

    Update no-deprecated-experimental-components.js (#333)
    * Update no-deprecated-experimental-components.js
    
    * test update
    
    * Create gentle-pants-taste.md

--- src/rules/no-deprecated-experimental-components.js
+++ src/rules/no-deprecated-experimental-components.js
@@ -55,7 +55,7 @@ module.exports = {
           // eslint-disable-next-line i18n-text/no-en
           const message = `The experimental ${components.join(', ')} ${
             components.length > 1 ? 'are' : 'is'
-          } deprecated. Please import from the stable entrypoint (@primer/react) if available, or check https://primer.style/product/components/ for alternative components.`
+          } deprecated. Please import from the stable entrypoint (@primer/react) if available. Check https://primer.style/product/getting-started/react/migration-guides/ for migration guidance or https://primer.style/product/components/ for alternative components.`
 
           context.report({
             node,
