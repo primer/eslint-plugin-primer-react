e33e87d237d77ade0d024cd1bd842524cc2738f8
Jon Rohan <yes@jonrohan.codes>
2025-08-07 21:52:33 +0000

    Update test for eslint v9

--- src/rules/__tests__/use-styled-react-import.test.js
+++ src/rules/__tests__/use-styled-react-import.test.js
@@ -2,11 +2,13 @@ const rule = require('../use-styled-react-import')
 const {RuleTester} = require('eslint')
 
 const ruleTester = new RuleTester({
-  parserOptions: {
+  languageOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
   },
 })
